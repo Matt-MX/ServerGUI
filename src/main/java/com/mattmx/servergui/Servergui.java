4009667d362a3aa20ed6e1fbf07706d8d52f09ad
MattMXX <mangrovethefox@outlook.com>
2022-11-12 22:29:00 +0000

    change ver

--- src/main/java/com/mattmx/servergui/Servergui.java
+++ src/main/java/com/mattmx/servergui/Servergui.java
@@ -33,7 +33,7 @@ import static com.mattmx.servergui.util.ServerConnection.connectPlayer;
 @Plugin(
         id = "servergui",
         name = "Servergui",
-        version = "1.1",
+        version = "2.0",
         description = "A GUI for all your servers!",
         url = "https://www.mattmx.com/",
         authors = {"MattMX"}
